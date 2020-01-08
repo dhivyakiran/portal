4e1b1fa358559b70b94bbe23417897c92151ccdb
dhivyakiran <dhivya.krish15@gmail.com>
2020-01-08 11:07:40 +0530

    Update and rename members/app.js to member/app.js

--- member/app.js
+++ member/app.js
@@ -12,7 +12,6 @@
                 userSSL: true,
                 apiVersion: 'beta'
             });
-
     }])
     .controller('MainController', ['$scope', 'baasicArticleService',
         function($scope, articleService) {
